03476133eeda9352f11f7a302fd52f384e89a1cc
Domingo <adomingor@hotmail.com>
2026-03-28 19:33:09 -0300

    Creando componentes livewire separados: Clase y Vista

---
+++ app/Livewire/ClaseSepradaDeVista.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ClaseSepradaDeVista extends Component
+{
+    public function render()
+    {
+        return view('livewire.clase-seprada-de-vista');
+    }
+}
